7cefabeb798d5013184c257f05fa6e0dc0669cd9
Ricky <rickdhanota@gmail.com>
2020-10-29 19:29:03 -0700

    initial commit

---
+++ findThreeLargestNumbers_easy.py
@@ -0,0 +1,10 @@
+#Find the 3 largest numbers
+#Write a function that takes in an array of at least 3 integers and, without sorting the input array, returns a sorted array of the three largest integers in the input array.
+
+#The function should return duplicate integers if necessary; for example, it should return [10, 10, 12] for an input array of [10, 5, 9, 10, 12]
+
+#array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+#[18, 141, 541]
+
+def findThreeLargestNumbers(array):
+    pass

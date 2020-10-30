7a46ef35c7e36a0e1ecfe337fc980939b03f01b8
Ricky <rickdhanota@gmail.com>
2020-10-29 20:34:44 -0700

    finished

--- findThreeLargestNumbers_easy.py
+++ findThreeLargestNumbers_easy.py
@@ -7,4 +7,24 @@
 #[18, 141, 541]
 
 def findThreeLargestNumbers(array):
-    pass
+    threeLargest = [None, None, None]
+    for num in array:
+        updateLargest(threeLargest, num)
+    return threeLargest
+
+def updateLargest(threeLargest, num):
+    if threeLargest[2] is None or num > threeLargest[2]:
+        shiftAndUpdate(threeLargest, num, 2)
+    elif threeLargest[1] is None or num > threeLargest[1]:
+        shiftAndUpdate(threeLargest, num, 1)
+    elif threeLargest[0] is None or num > threeLargest[0]:
+        shiftAndUpdate(threeLargest, num, 0)
+
+def shiftAndUpdate(array, num, idx):
+    for i in range(idx + 1): #python its exclusive so we have to do idx+1 to account for the range function
+        if i == idx:
+            array[i] = num
+        else:
+            array[i] = array[i + 1]
+
+print(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
